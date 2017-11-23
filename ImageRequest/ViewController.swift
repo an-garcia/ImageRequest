ed09d2bb9495a879a5cba670d5b38d2d8497fbdd
Angel Newton <xanewton@gmail.com>
2017-11-23 14:34:06 -0500

    Allow http images.

--- ImageRequest/ViewController.swift
+++ ImageRequest/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg")!
+        //let imagePath = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg"
+        let imagePath = "http://www.spectra-physics.com/assets/components/phpthumbof/cache/twitter-2016-11-nature_spitfire_article.72848845b1fe301dafc37322de44c90f.jpg"
+        let imageURL = URL(string: imagePath)!
         
         // use "URLSession.shared" to access the shared URLSession
         let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
