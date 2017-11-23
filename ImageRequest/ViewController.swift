3f61170c7ac251e98317bea08139ac25d709d0af
Angel Newton <xanewton@gmail.com>
2017-11-23 13:59:55 -0500

    Query the image and show it

--- ImageRequest/ViewController.swift
+++ ImageRequest/ViewController.swift
@@ -17,8 +17,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg")!
-        // use "URLSession.shared" to access the shared URLSession
         
+        // use "URLSession.shared" to access the shared URLSession
+        let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if error == nil {
+                let downloadImage = UIImage(data: data!)
+                performUIUpdatesOnMain {
+                    self.imageView.image = downloadImage
+                }
+            } else {
+                print("Error loading image")
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
